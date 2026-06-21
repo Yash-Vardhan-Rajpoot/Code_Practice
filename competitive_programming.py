a689fb998785dfb7167a88484b66af54d3cbe1c4
Yash-Vardhan-Rajpoot <yashvardhanrajpoot15@gmail.com>
2026-06-21 23:36:30 +0530

    update competitive_programming.py

--- competitive_programming.py
+++ competitive_programming.py
@@ -1,6 +1,12 @@
-def helper(n:int,s:str)->str:
-    
+from math import ceil
+def helper(n:int,x:int,arr:list)->int:
+    ans=[]
+    for i in range(n):
+        ans.append(ceil(arr[i]/x))
+    print(ceil(sum(arr)/x),sum(ans)) 
 t=int(input())
 while t>0:
-    
+    n,x=map(int,input().split())
+    arr=list(map(int,input().split()))
+    helper(n,x,arr)
     t-=1
