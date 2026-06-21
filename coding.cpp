8997d5a2b291e8cb3f3e71c563ff01712dbf15f8
Yash-Vardhan-Rajpoot <yashvardhanrajpoot15@gmail.com>
2026-06-21 23:37:10 +0530

    update coding.cpp

--- coding.cpp
+++ coding.cpp
@@ -1,49 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solve(){
-    int n;
-    cin>>n;
-    string str;
-    cin>>str;
-    map<char,int> mp;
-    for(char &ch:str) mp[ch]++;
-    char chmax,chmin;
-    int maxi=INT_MIN;
-    int mini=INT_MAX;
-    for(auto &it:mp){
-        if(it.second>maxi){
-            maxi=it.second;
-            chmax=it.first;
+int helper(vector<int>&arr,int n,int x){
+    int count=0;
+    vector<int> pref(n);
+    pref[0]=arr[0];
+    for(int i=1;i<n;i++) pref[i]=pref[i-1]+arr[i];
+    for(int i=0;i<n;i++){
+        int sum=pref[i];
+        vector<int> check;
+        while(sum>0){
+            check.push_back(sum%10);
+            sum/=10;
         }
-        if(it.second<=mini){
-            mini=it.second;
-            chmin=it.first;
-        }
+        if(check[0]==x and check[check.size()-1]==x) count+=1;
     }
-    if(mini==maxi){
-        for(int i=0;i<n-1;i++){
-            if(str[i]!=str[i+1]){
-                str[i+1]=str[i];
-                break;
+    int temp=pref[0];
+    for(int i=1;i<n;i++){
+        for(int j=i;j<n;j++){
+            int sum=pref[j]-temp;
+            vector<int> check;
+            while(sum>0){
+                check.push_back(sum%10);
+                sum/=10;
             }
+            if(check[0]==x and check[check.size()-1]==x) count+=1;
         }
-    }else{
-        for(int i=0;i<n;i++){
-            if(mp[str[i]]==mini) {
-                str[i]=chmax;
-                break;
-            }
-        }
+        temp=pref[i];
     }
-    cout<<str<<" "<<endl;
+    return count;
 }
-int main() {
+int main(){
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
+    cin.tie(nullptr);
+    int n,x;
+    cin>>n>>x;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    cout<<helper(arr,n,x)<<endl;
     return 0;
 }
