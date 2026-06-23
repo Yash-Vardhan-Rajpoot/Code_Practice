0b8763cd85a3e5a213e63a891cb4b52453a7e806
Yash-Vardhan-Rajpoot <yashvardhanrajpoot15@gmail.com>
2026-06-23 23:36:24 +0530

    update coding.cpp

--- coding.cpp
+++ coding.cpp
@@ -1,33 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-string helper(string& str){
-    int n=str.size();
-    set<char> _set;
-    for(int i=0;i<n;i++) _set.insert(str[i]);
-    char ch1,ch2;
-    for(auto &it:_set){
-        for(int i=0;i<n;i++){
-            if(it==str[i]) break;
-            if(str[i]>it){
-                ch1=it;
-                ch2=str[i];
-                break;
-            }
-        }
+string helper(int n,int k,string& str){
+    unordered_map<char,int> mp;
+    for(char &ch:str) mp[ch]++;
+    int count=0;
+    for(auto &it:mp){
+        int freq=it.second;
+        if(freq%2!=0) count+=1;
     }
-    string ans="";
-    for(int i=0;i<n;i++){
-        if(str[i]==ch2) ans+=ch1;
-        else if(str[i]==ch1) ans+=ch2;
-        else ans+=str[i];
-    }
-    return ans;
+    int compodd=count-1;
+    if(compodd>k) return "NO";
+    return "YES";
 }
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    string str;
-    cin>>str;
-    cout<<helper(str)<<endl;
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        string str;
+        cin>>str;
+        cout<<helper(n,k,str)<<endl;    
+    }
     return 0;
 }
