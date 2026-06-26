fd335b9ee521d7fc6f3fa45499478c1d55767416
Yash-Vardhan-Rajpoot <yashvardhanrajpoot15@gmail.com>
2026-06-27 02:28:23 +0530

    update coding.cpp

--- coding.cpp
+++ coding.cpp
@@ -1,12 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> helper(int a,int b,int c){
-    int a1=0,b1=0,c1=0;
-    if((a&1)==(b&1)) c1=1;
-    if((a&1)==(c&1)) b1=1;
-    if((b&1)==(c&1)) a1=1;
-    return {a1,b1,c1};
-
+pair<int,int> helper(int n,int h1,int m1,vector<pair<int,int>>&t){
+    bool check=false;
+    for(auto &p:t){
+        int h2=p.first;
+        int m2=p.second;
+        if(h1==h2 and m1==m2){
+            check=true;
+            break;
+        }
+    }
+    if(check) return {0,0};
+    else{
+        vector<pair<int,int>> p;
+        for(auto &pa:t){
+            int h2=pa.first;
+            int m2=pa.second;
+            if(h1<h2){
+                if(m2>=m1) p.push_back({h2-h1,m2-m1});
+                else p.push_back({h2-h1-1,m2-m1+60});
+            }
+            else if(h1==h2){
+                if(m2>m1) p.push_back({h2-h1,m2-m1});
+                else{
+                    int addh=0,addm=0;
+                    if(m2==0) addh=24-h1;
+                    else{
+                        addh=23-h1;
+                        addm=60-m1;
+                    }
+                    addm+=m2;
+                    if(addm>=60){
+                        addh+=1;
+                        addm%=60;
+                    }
+                    p.push_back({addh+h2,addm});
+                }
+            }
+            else{
+                int addh=0,addm=0;
+                if(h2!=0 and m2==0) addh=24-h1;
+                else{
+                    addh=23-h1;
+                    addm=60-m1;
+                }
+                addm+=m2;
+                if(addm>=60){
+                    addh+=1;
+                    addm%=60;
+                }
+                p.push_back({addh+h2,addm});
+            }
+        }
+        sort(begin(p),end(p));
+        return {p[0].first,p[0].second};
+    }
 }
     
 int main(){
@@ -15,12 +63,16 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int a,b,c;
-        cin>>a>>b>>c;
-        vector<int> ans;
-        ans=helper(a,b,c);
-        for(int i=0;i<ans.size();i++) cout<<ans[i]<<" ";
-        cout<<endl;
+        int n,h,m;
+        cin>>n>>h>>m;
+        vector<pair<int,int>>t;
+        for(int i=0;i<n;i++){
+            int h1,m1;
+            cin>>h1>>m1;
+            t.push_back({h1,m1});
+        }
+        pair<int,int> ans=helper(n,h,m,t);
+        cout<<ans.first<<" "<<ans.second<<endl;
     }
     return 0;
 }
